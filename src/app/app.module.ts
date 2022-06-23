299e52ae48b137ffb82e0950c838f5b97e337cf5
moy314 <moy5912@gmail.com>
2022-06-22 20:04:23 -0500

    5:agregando listado y funcion de borrado con ngfor y ngof

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { contadorComponent } from './contadores/contador.component';
 import { heroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     contadorComponent,
-    heroeComponent
+    heroeComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
