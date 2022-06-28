9d80198d5692480e2a693432665d95c7ca8683fb
moy314 <moy5912@gmail.com>
2022-06-28 12:17:35 -0500

    7:agregando ContadorModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contadorComponent } from './contadores/contador.component';
+import { ContadorModule } from './contadores/contador/contador.modulo';
 import { heroesModule } from './heroes/heroes.modulo';
-// import { heroeComponent } from './heroes/heroe/heroe.component';
-// import { ListadoComponent } from './heroes/listado/listado.component';
+// import { heroeComponent } from './herstado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    contadorComponent,
-    // heroeComponent,
-    // ListadoComponent
   ],
   imports: [
     BrowserModule,
     heroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
