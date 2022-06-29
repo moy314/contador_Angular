ab2df14e7098e22ddc8de292900ef01fd060a817
moy314 <moy5912@gmail.com>
2022-06-29 13:43:55 -0500

    9: agregando formsmodule - ngsubmit

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,18 +1,26 @@
+//importaciones de angular
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+//importaciones de terceros...
+
+// importaciones del usuario
 import { MainPageComponent } from './main-page/main-page.component';
 
 
 
 @NgModule({
   declarations: [
+    
     MainPageComponent
   ],
   exports:[
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
